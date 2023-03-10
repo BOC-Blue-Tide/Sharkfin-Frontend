00ae161fa5ac3a177ffe48c4920abeeda8fb1d16
danielhalper <daniel@stepuptutoring.org>
2023-03-09 22:48:00 -0800

    Added MUI functionality

---
+++ client/src/components/exampleComponent.jsx
@@ -0,0 +1,11 @@
+import { Button } from '@mui/material';
+
+function MyComponent() {
+  return (
+    <Button variant="contained" color="primary">
+        Example Component using MUI...
+    </Button>
+  );
+}
+
+export default MyComponent;

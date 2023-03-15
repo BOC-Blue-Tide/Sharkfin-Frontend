1301d6bf5976e621f8bb8a98008acc5fb4747ec8
danielhalper <daniel@stepuptutoring.org>
2023-03-14 18:53:40 -0700

    Accountinfo changes

--- client/src/components/accountInfo.jsx
+++ client/src/components/accountInfo.jsx
@@ -1,17 +1,19 @@
 import React, { useState } from 'react';
-import { Button } from '@mui/material';
-import { Grid } from '@mui/material';
+import { Button, Input } from '@mui/material';
+import { Grid, Card } from '@mui/material';
 import { Link, Navigate } from 'react-router-dom';
+import { width } from '@mui/system';
 
 
 
-// const style = {
-//     // layout: {
-//     //         display: "grid",
-//     //         gridTemplateColumns: "repeat(2, 1fr)",
-//     //         gridTemplateRows: "1fr",
-//     //         }
-// }
+const style = {
+   gridCard: {
+      margin: "40px",
+      backgroundColor: "lightGray",
+      width: "100%",
+      height: "100%"
+   }
+}
             // <div sx={style.layout}>
 
 
@@ -20,11 +22,20 @@ function AccountInfo() {
 
         return (
             <Grid container spacing={2}>
+               <Grid item xs={6} sm={6}>
+                  <h1>Account Information</h1>
+                  <h4>You have no funds! Connect your bank account to get started...</h4>
                 <Link to="/">
                 <Button variant="contained" color="primary">
                    Go Home
                 </Button>
                 </Link>
+                </Grid>
+                <Grid item xs={6} sm={6}>
+                  <Card sx={style.gridCard}>
+                <Input>Some content here</Input>
+                </Card>
+                </Grid>
              </Grid>
         );
 }

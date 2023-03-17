583d47fd60746312111e383c8b05141e3aa0bf35
uestczmn <uestczmn@gmail.com>
2023-03-17 15:52:43 -0700

    add Friends components: AddFriends and ViewRequest

---
+++ mockData_friends.js
@@ -0,0 +1,35 @@
+let friends = [
+  {
+    id: 1,
+    username: 'Rachel'
+  },
+  {
+    id: 2,
+    username: 'Monica'
+  },
+  {
+    id: 3,
+    username: 'Phoebe'
+  },
+  {
+    id: 4,
+    username: 'Chandler'
+  },
+  {
+    id: 5,
+    username: 'Joey'
+  },
+  {
+    id: 6,
+    username: 'Ross'
+  }
+];
+
+let searchResults = [
+  {
+    id: 1,
+    username: 'Rachel'
+  }
+];
+
+export { friends, searchResults }
